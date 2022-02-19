d2a84dcb51f3dbe8fb0eed063e57ec67c5ed0a45
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-02-20 01:29:50 +0200

    Update fetchImages.js

--- src/js/api/fetchImages.js
+++ src/js/api/fetchImages.js
@@ -4,9 +4,6 @@ import apiSettings from './settings';
 const { BASE_URL, API_KEY, TYPE, PER_PAGE } = apiSettings;
 
 export const fetchImages = (name, galleryPage) => {
-  console.log(
-    `${BASE_URL}${API_KEY}&q=${name}${TYPE}&orientation=horizontal&safesearch=true&page=${galleryPage}&${PER_PAGE}`,
-  );
   return axios.get(
     `${BASE_URL}${API_KEY}&q=${name}${TYPE}&orientation=horizontal&safesearch=true&page=${galleryPage}&${PER_PAGE}`,
   );
